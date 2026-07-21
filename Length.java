136fa7bd76a899b9b81b26fdacd194b6fbff2d43
tejashreeumashankar <tejashreeumashankar@gmail.com>
2026-07-21 10:18:05 +0530

    fourth push

---
+++ Length.java
@@ -0,0 +1,14 @@
+class Length {
+    public int lengthOfLastWord(String s)     
+    {
+        s=s.trim();
+        String[] words =s.split(" ");
+        return words[words.length-1].length();
+        
+    }
+}
+            
+            
+                        
+            
+            
