ce3c41795c5b728f458dcef51da6d7000eab7959
tejashreeumashankar <tejashreeumashankar@gmail.com>
2026-07-23 11:43:51 +0530

    now

---
+++ Square.java
@@ -0,0 +1,9 @@
+class Square {
+    public boolean squareIsWhite(String coordinates) {
+        char col = coordinates.charAt(0); //a
+        char row = coordinates.charAt(1); //1
+        int colNumber = col - 'a' + 1;
+        int rowNumber = row - '0';
+        return (colNumber + rowNumber) % 2 != 0;
+    }
+}
