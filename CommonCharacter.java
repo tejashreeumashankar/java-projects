30a5285214b4603da77395377661ca365a29e228
tejashreeumashankar <tejashreeumashankar@gmail.com>
2026-07-23 08:40:46 +0530

    question

---
+++ CommonCharacter.java
@@ -0,0 +1,30 @@
+class CommonCharacter {
+    public List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+
+        for (char c : words[0].toCharArray()) {
+            minFreq[c - 'a']++;
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            int[] currentFreq = new int[26];
+            for (char c : words[i].toCharArray()) {
+                currentFreq[c - 'a']++;
+            }
+
+            for (int j = 0; j < 26; j++) {
+                minFreq[j] = Math.min(minFreq[j], currentFreq[j]);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i] > 0) {
+                result.add(String.valueOf((char) (i + 'a')));
+                minFreq[i]--;
+            }
+        }
+
+        return result;
+    }
+}
